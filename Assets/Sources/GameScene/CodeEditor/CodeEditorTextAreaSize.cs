4e29c7bbf81a658a1b324719bf4e8af1e8c064ae
salmonBowl <kcfunakoshi@gmail.com>
2025-06-24 14:29:44 +0900

    update class TextAreaSize

--- Assets/Sources/GameScene/CodeEditor/CodeEditorTextAreaSize.cs
+++ Assets/Sources/GameScene/CodeEditor/CodeEditorTextAreaSize.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class CodeEditorTextAreaSize
 {
+    private readonly CodeEditorLineCountManager lineCountManager;
+    private readonly CodeEditorSettings settings;
 
+    private readonly RectTransform areaVoidStart;
+    private readonly RectTransform areaVoidUpdate;
 
-    [SerializeField] RectTransform areaVoidStart;
-    [SerializeField] RectTransform areaVoidUpdate;
+    public CodeEditorTextAreaSize(
+        CodeEditorLineCountManager lineCountManager,
+        CodeEditorSettings settings,
+        RectTransform areaVoidStart,
+        RectTransform areaVoidUpdate
+        )
+    {
+        this.lineCountManager = lineCountManager;
+        this.settings = settings;
+
+        this.areaVoidStart = areaVoidStart;
+        this.areaVoidUpdate = areaVoidUpdate;
+    }
 
     public void Execute()
     {
