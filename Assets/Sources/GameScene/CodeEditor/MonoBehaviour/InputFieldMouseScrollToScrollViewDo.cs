0da77c31838947dc0de92556f17410ca54a23287
salmonBowl <kcfunakoshi@gmail.com>
2025-06-25 08:59:57 +0900

    write summary

--- Assets/Sources/GameScene/CodeEditor/MonoBehaviour/InputFieldMouseScrollToScrollViewDo.cs
+++ Assets/Sources/GameScene/CodeEditor/MonoBehaviour/InputFieldMouseScrollToScrollViewDo.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// ScrollViewの子になっているInputFieldにアタッチします
+/// </summary>
 public class InputFieldMouseScrollToScrollViewDo : MonoBehaviour
 {
     [SerializeField] ScrollRect parentScrollView;
@@ -12,8 +15,16 @@ public class InputFieldMouseScrollToScrollViewDo : MonoBehaviour
             Debug.LogError("InputField.IFMSTSVD : parentScrollViewがアタッチされていません");
     }
 
+    /// <summary>
+    /// InputField上でスクロールが発生したら呼び出されます
+    /// </summary>
+    /// <param name="e">InputFieldのイベントデータ</param>
     public void OnScroll(PointerEventData e)
     {
+        // 親のScrollViewにイベントを渡します
         ExecuteEvents.Execute(parentScrollView.gameObject, e, ExecuteEvents.scrollHandler);
+
+        // InputFieldのスクロールを無効化
+        e.Use();
     }
 }
