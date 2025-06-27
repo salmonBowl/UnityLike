6b185ef78c2aa5d56c4034f6ae4d13d1e2357343
salmonBowl <kcfunakoshi@gmail.com>
2025-06-27 17:49:51 +0900

    update class TitleSceneAll

--- Assets/Sources/TitleScene/TitleSceneAll.cs
+++ Assets/Sources/TitleScene/TitleSceneAll.cs
@@ -1,7 +1,7 @@
 ﻿
 /*
     タイトルのシーンの処理を担当するクラス
-    全く責務分離してないけどシーンがやること少ないのでゆるして
+    シーンがやること少ないのでこれだけで完結させるクラス、素晴らしい
  */
 
 using UnityEngine;
@@ -13,24 +13,33 @@ using System;
 public class TitleSceneAll : MonoBehaviour
 {
     [SerializeField] Image panel;
-    [SerializeField] Button buttonEldenEnd;
-    [SerializeField] TextMeshProUGUI textEldenEnd;
+    [SerializeField] Button buttonNewStart;
+    [SerializeField] Button buttonLoad;
+
+    [SerializeField] RectTransform UnityIcon;
 
     bool IsTitleUpdate;
     float panelAlpha;
-    int counterPressEnd;
+
+    bool selectedButton = false;
 
     void Start()
     {
         Application.targetFrameRate = 60;
         IsTitleUpdate = true;
         panelAlpha = 0f;
-        counterPressEnd = 0;
-        buttonEldenEnd.interactable = true;
+        buttonNewStart.interactable = true;
+        buttonLoad.interactable = true;
     }
     void Update()
     {
-        if (!IsTitleUpdate)
+        if (IsTitleUpdate)
+        {
+            float angleDegDestination = selectedButton? 120 : 0;
+
+            UnityIcon.rotation = Quaternion.Lerp(Quaternion.Euler(0, 0, angleDegDestination), UnityIcon.rotation, 0.1f);
+        }
+        else // シーン遷移のフェードアウト
         {
             if (panelAlpha == 1)
             {
@@ -53,45 +62,12 @@ public class TitleSceneAll : MonoBehaviour
         panel.color = panelColor;
     }
 
-    public void EldenStart()
+    public void CreateNewProject()
     {
         IsTitleUpdate = false;
     }
-    public void EldenEnd()
+    public void LoadProject()
     {
-        string dispMessage;
-
-        counterPressEnd++;
-        switch (counterPressEnd)
-        {
-            case 0:
-                dispMessage = "エルデン終了";
-                break;
-            case 1:
-                dispMessage = "本当に終了しますか?";
-                break;
-            case 2:
-                dispMessage = "え、本当に終了しますか?";
-                break;
-            case 3:
-                dispMessage = "なぜ終了しますか?";
-                break;
-            case 4:
-                dispMessage = "私の提供したエルデンリングはそんなに面白くありませんでしたか?";
-                break;
-            case 5:
-                DateTime today = DateTime.Now;
-                dispMessage = $"{today.Month}月{today.Day}日：次回、このメッセージはもう表示されないかもしれません。1分でいいので、どうか読み飛ばさないでください。今日は{today.Month}月{today.Day}日で、私たち非営利団体は、あなたのご支援を必要としています。 これはとても重要なことなのです。エルデンリングが設立されたとき、それは広告なしで無償でプレイできる最初のオンラインスペースのひとつでした。この場所はあなたのものです。寄付をしてくださるのはプレイヤーのわずか2％です。¥300でも¥2,000でも、あなたのご寄付には価値があります。 エルデンリングとその姉妹ゲームを運営するエルデン財団より";
-                break;
-            case 6:
-                dispMessage = "エルデン終了に失敗しました";
-                buttonEldenEnd.interactable = false;
-                break;
-            default:
-                dispMessage = "エルデン終了はエラーです";
-                break;
-        }
-
-        textEldenEnd.text = dispMessage;
+        IsTitleUpdate = false;
     }
 }
