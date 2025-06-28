638833926ca5ae6c38e863402e9ad4782ecc2593
salmonBowl <kcfunakoshi@gmail.com>
2025-06-28 22:13:49 +0900

    write logic skipping whitespace

--- Assets/Sources/UseCases/Compiler/ILexer.cs
+++ Assets/Sources/UseCases/Compiler/ILexer.cs
@@ -5,21 +5,6 @@ namespace UnityLike.UseCases.Compiler
     public interface ILexer
     {
         /// <summary>
-        /// 次の文字が何なのかを取得します
-        /// </summary>
-        char Peek();
-
-        /// <summary>
-        /// 現在の文字を消費し、読み取り位置を1つ先に進めます
-        /// </summary>
-        void Consume();
-
-        /// <summary>
-        /// ファイルを読み取りが終端に達したかを判定します
-        /// </summary>
-        bool IsEndOfFile();
-
-        /// <summary>
         /// 次のトークンを生成し返します
         /// 字句解析の主要なメソッドです
         /// </summary>
--- Assets/Sources/UseCases/Compiler/Lexer.cs
+++ Assets/Sources/UseCases/Compiler/Lexer.cs
@@ -25,6 +25,23 @@ namespace UnityLike.UseCases.Compiler
         }
 
         /// <summary>
+        /// 次のトークンを生成して返します
+        /// このメソッドは字句解析の主要な部分になりますが、現時点では未実装です
+        /// </summary>
+        public Token GetNextToken()
+        {
+            if (IsEndOfFile())
+            {
+                return new Token(TokenType.EOF, "\0", currentLine, currentColumn);
+            }
+
+            char nextChar = Peek();
+            Consume();
+
+            return new Token(TokenType.Unknown, "", currentLine, currentColumn);
+        }
+
+        /// <summary>
         /// 読み取り位置は進めず、先読みして次の文字を返します
         /// </summary>
         public char Peek()
@@ -41,11 +58,11 @@ namespace UnityLike.UseCases.Compiler
         /// <summary>
         /// 現在の文字を消費し、読み取り位置を1つ先に進めます
         /// </summary>
-        /// <exception cref="InvalidOperationException">ファイルが終端に達している状態で読み取りを進めようとしています</exception>
+        /// <exception cref="InvalidOperationException"></exception>
         public void Consume()
         {
             if (IsEndOfFile())
-                throw new InvalidOperationException("Compiler.Lexer:ILexer.Consume() : これ以上読み取り位置を進められません");
+                throw new InvalidOperationException("Compiler.Lexer:ILexer.Consume() : これ以上読み取りを進められません");
 
             if (sourceCode[currentIndex] == '\n')
             {
@@ -66,28 +83,14 @@ namespace UnityLike.UseCases.Compiler
             }
         }
 
+        /// <summary>
+        /// ファイルの読み取り位置が終端に達したかを判定します
+        /// </summary>
         public bool IsEndOfFile()
         {
             return (sourceCode.Length <= currentIndex);
             // ラムダ式ではなく不等号
             // 等号でないのは例外を避けるため
-        }
-
-        /// <summary>
-        /// 次のトークンを生成して返します
-        /// このメソッドは字句解析の主要な部分になりますが、現時点では未実装です
-        /// </summary>
-        public Token GetNextToken()
-        {
-            if (IsEndOfFile())
-            {
-                return new Token(TokenType.EOF, "\0", currentLine, currentColumn);
-            }
-
-            char nextChar = Peek();
-            Consume();
-
-            return new Token(TokenType.Unknown, "", currentLine, currentColumn);
         }
     }
 }
