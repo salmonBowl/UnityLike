c63dc676236e8faf15cd08d3cc1d26185afe5d88
salmonBowl <kcfunakoshi@gmail.com>
2025-06-28 20:09:03 +0900

    created interface ILexer

---
+++ Assets/Sources/UseCases/Compiler/ILexer.cs
@@ -0,0 +1,27 @@
+
+namespace UnityLike.UseCases.Compiler
+{
+    public interface ILexer
+    {
+        /// <summary>
+        /// 次の文字が何なのかを取得します
+        /// </summary>
+        char Peek();
+
+        /// <summary>
+        /// 現在の文字を消費し、読み取り位置を1つ先に進めます
+        /// </summary>
+        void Consume();
+
+        /// <summary>
+        /// 次のトークンを生成し返します
+        /// 字句解析の主要なメソッドです
+        /// </summary>
+        Token GetNextToken();
+
+        /// <summary>
+        /// ファイルを読み取りが終端に達したかを判定します
+        /// </summary>
+        bool IsEndOfFile();
+    }
+}
