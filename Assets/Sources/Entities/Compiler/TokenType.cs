c566a6c5a6ef05d8f9a4b977aff3139ab2f07b89
salmonBowl <kcfunakoshi@gmail.com>
2025-06-29 20:10:42 +0900

    add TokenType

--- Assets/Sources/Entities/Compiler/TokenType.cs
+++ Assets/Sources/Entities/Compiler/TokenType.cs
@@ -7,6 +7,27 @@ namespace UnityLike.Entities.Compiler
 
         NumberLiteral, // 数字
 
+        // 算術演算子
+        Plus,
+        Minus,
+        Multiply,
+        Divide,
+
+        // 代入演算子
+        Equals,
+        PlusEquals,
+        MinusEquals,
+        MultiplyEquals,
+        DivideEquals,
+
+        // 比較演算子
+        EqualEquals,
+        NotEquals,
+        LessThan,
+        LessThanOrEqual,
+        GreaterThan,
+        GreaterThanOrEqual,
+
         Unknown, // 入力中や入力ミスなど
 
         EOF // 終端文字トークン
