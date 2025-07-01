4b8bb7623f66f57e871d89b607dba58340341bab
salmonBowl <kcfunakoshi@gmail.com>
2025-07-01 14:45:15 +0900

    add some TokenType

--- Assets/Sources/Entities/Compiler/TokenType.cs
+++ Assets/Sources/Entities/Compiler/TokenType.cs
@@ -30,9 +30,41 @@ namespace UnityLike.Entities.Compiler
         LessThan,
         LessThanOrEqual,
 
+        /*
+            括弧類 まで新規追加
+            これのシンタックスハイライト部分はまだ実装されていません
+         */
+
+        // 関数実行のために必要なもの
+        COLON,
+        COMMA,
+
+        // 組み込み型 (青色で表示します)
+        TYPE_STANDARD, //intやfloatなどの変数で、voidなどの関数宣言はこのエンジンで使われない
+
+        // キーワード
+        IF,
+        ELSE,
+        FOR,
+        WHILE,
+        NEW,
+        NULL,
+        TRUE,
+        FALSE,
+        PUBLIC,
+        PRIVATE,
+
+        // 括弧類
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        LEFT_BRACKET,
+        RIGHT_BRACKET,
+    
         Unknown, // 入力中や入力ミスなど
 
         Return, // 改行トークン
-        EOF // 終端文字トークン
+        EOF // 終端トークン
     }
 }
